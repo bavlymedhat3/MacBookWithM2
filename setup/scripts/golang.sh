1e787a932251f0b9a300075b30723781040ddf4e
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-10-01 21:49:42 -0700

    update goenv

--- setup/scripts/golang.sh
+++ setup/scripts/golang.sh
@@ -5,11 +5,13 @@ echo -e "\n# Golang" >> ~/.bashrc
 echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.bashrc
 echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(goenv init -)"' >> ~/.bashrc
+echo 'export PATH="$GOROOT/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bashrc
 
 source ~/.bashrc
 
 goenv install -l
-goenv install 1.19.3
-goenv global 1.19.3
+goenv install 1.21.1
+goenv global 1.21.1
 
 # go env -w GO111MODULE=on
