30b033eb69bc4e29f343d3ed2b4dcf7239bfceaa
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-10-04 19:55:35 -0700

    update awscliv2 and kubectl

--- setup/scripts/aws_cli_v2.sh
+++ setup/scripts/aws_cli_v2.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-sudo installer -pkg AWSCLIV2.pkg -target /
+# Don't use below command to prevent from installing "Rosetta 2". Instead, just use homebrew
+# curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+# sudo installer -pkg AWSCLIV2.pkg -target /
+
+brew install awscli@2
--- setup/scripts/kubectl.sh
+++ setup/scripts/kubectl.sh
@@ -4,8 +4,8 @@ SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}) && pwd)
 cd ${SOURCE_DIR}
 
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/
-curl -LO "https://dl.k8s.io/release/v1.27.2/bin/darwin/arm64/kubectl"
-curl -LO "https://dl.k8s.io/release/v1.27.2/bin/darwin/arm64/kubectl.sha256"
+curl -LO "https://dl.k8s.io/release/v1.28.2/bin/darwin/arm64/kubectl"
+curl -LO "https://dl.k8s.io/release/v1.28.2/bin/darwin/arm64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | shasum -a 256 --check
 chmod +x ./kubectl
 
